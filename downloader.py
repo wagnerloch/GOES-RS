a3edf44fe29ab0ef9337ef262aafc54bf2bee11f
Wagner Loch <wagner_loch@hotmail.com>
2025-03-20 07:55:01 -0300

    Changed for RS

--- downloader.py
+++ downloader.py
@@ -1,7 +1,6 @@
 import GOES
 
 GOES.download('goes16', 'GLM-L2-LCFA', 
-              DateTimeIni='20240501-000000', 
-              DateTimeFin='20240503-235959',
-              path_out='Downloads/',
-              rename_fmt='%Y%m%d%H%M%S')
+              DateTimeIni='20240425-000000', 
+              DateTimeFin='20240512-235959',
+              path_out='Downloads/')
